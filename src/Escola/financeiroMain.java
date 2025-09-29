6f5a1f207140f320cfab1bd6dd815760e1d9ac13
Mdrvinicius <wfqawsfqw@gmail.com>
2025-09-29 16:20:29 -0300

    comit 3

---
+++ src/Escola/financeiroMain.java
@@ -0,0 +1,38 @@
+package Escola;
+
+class Financeiro {
+
+    private double mensalidade;
+
+
+    public double getMensalidade() {
+        return mensalidade;
+    }
+
+    public void setMensalidade(double mensalidade) {
+        if (mensalidade > 0) {
+            this.mensalidade = mensalidade;
+        } else {
+            System.out.println("Valor de mensalidade inválido");
+        }
+    }
+
+    public void pagarMensalidade(double pagamento) {
+        if (pagamento == mensalidade) {
+            System.out.println("Mensalidade de R$" + pagamento + " paga com sucesso!");
+        } else {
+            System.out.println("Valor pago diferente da mensalidade.");
+        }
+    }
+}
+
+
+public class financeiroMain {
+    public static void main(String[] args) {
+        Financeiro meuFinanceiro = new Financeiro();
+        meuFinanceiro.setMensalidade(1200.00);
+        System.out.println("Mensalidade cadastrada: R$" + meuFinanceiro.getMensalidade());
+        meuFinanceiro.pagarMensalidade(1200.00);
+
+    }
+}
